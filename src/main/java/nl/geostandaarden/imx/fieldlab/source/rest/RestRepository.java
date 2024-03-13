e8b5e0a23da482c7e0bdb6c3e9b8c984a3bd9365
Joost Farla <joost@qonduct.io>
2024-03-13 14:34:41 +0100

    Get persoon

--- src/main/java/nl/geostandaarden/imx/fieldlab/source/rest/RestRepository.java
+++ src/main/java/nl/geostandaarden/imx/fieldlab/source/rest/RestRepository.java
@@ -5,6 +5,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.json.JsonMapper;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
@@ -12,8 +14,11 @@ import nl.geostandaarden.imx.orchestrate.engine.exchange.CollectionRequest;
 import nl.geostandaarden.imx.orchestrate.engine.exchange.DataRequest;
 import nl.geostandaarden.imx.orchestrate.engine.exchange.ObjectRequest;
 import nl.geostandaarden.imx.orchestrate.engine.source.DataRepository;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.netty.ByteBufFlux;
 import reactor.netty.http.client.HttpClient;
 
 @RequiredArgsConstructor
@@ -29,8 +34,7 @@ public class RestRepository implements DataRepository {
   @Override
   public Mono<Map<String, Object>> findOne(ObjectRequest request) {
     if ("Persoon".equals(request.getObjectType().getName())) {
-      var bsn = request.getObjectKey().get("bsn");
-      return Mono.just(Map.of("burgerservicenummer", bsn, "naam", "Odessa de Jager"));
+      return getPersoon(request);
     }
 
     return httpClient.get()
@@ -52,6 +56,49 @@ public class RestRepository implements DataRepository {
         .flatMapMany(resource -> Flux.fromIterable(resource.getData()))
         .map(JsonMapFlattener::flatten)
         .onErrorComplete();
+  }
+
+  private Mono<Map<String, Object>> getPersoon(ObjectRequest request) {
+    var bsn = request.getObjectKey().get("bsn");
+
+    var requestBody = new HashMap<String, Object>();
+    requestBody.put("type", "RaadpleegMetBurgerservicenummer");
+    requestBody.put("fields", List.of("naam"));
+    requestBody.put("burgerservicenummer", List.of(bsn));
+
+    return httpClient.headers(builder -> builder.set(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE))
+        .post()
+        .uri(getCollectionURI(request))
+        .send(ByteBufFlux.fromString(Mono.just(writeString(requestBody))))
+        .responseContent()
+        .aggregate()
+        .asString()
+        .map(responseString -> {
+          var responseBody = readString(responseString);
+          var persoon = ((List<Map<String, Object>>) responseBody.get("personen")).get(0);
+          var naam = (Map<String, Object>) persoon.get("naam");
+
+          var result = new HashMap<String, Object>();
+          result.put("burgerservicenummer", bsn);
+          result.put("naam", naam.get("volledigeNaam"));
+          return result;
+        });
+  }
+
+  private Map<String, Object> readString(String data) {
+    try {
+      return jsonMapper.readValue(data, Map.class);
+    } catch (IOException e) {
+      throw new RuntimeException("Could not read JSON.", e);
+    }
+  }
+
+  private String writeString(Object data) {
+    try {
+      return jsonMapper.writeValueAsString(data);
+    } catch (IOException e) {
+      throw new RuntimeException("Could not read JSON.", e);
+    }
   }
 
   private String getObjectURI(ObjectRequest request) {
